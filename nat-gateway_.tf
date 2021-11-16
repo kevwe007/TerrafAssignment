4780693c3e12612735acce262cf6691f1ec07c6c
kevwe007 <ojarikreh@yahoo.com>
2021-11-16 08:27:16 -0600

    coomit to main

--- nat-gateway_.tf
+++ nat-gateway_.tf
@@ -2,10 +2,7 @@
 
   resource "aws_eip" "new-nat1" {
 }
-resource "aws_eip" "new-nat2" {
-}
-resource "aws_eip" "new-nat3" {
-}
+
 
 # CREATE NAT GATEWAY in US-East-2A
 
@@ -15,27 +12,6 @@ resource "aws_eip" "new-nat3" {
 
   tags = {
     Name        = "Nat Gateway-2a"
-    Terraform   = "True"
-  }
-}
-# CREATE NAT GATEWAY in US-East-2B
-
-resource "aws_nat_gateway" "new-nat-gateway-2b" {
-  allocation_id = aws_eip.new-nat2.id
-  subnet_id     = aws_subnet.new-public-2b.id
-
-  tags = {
-    Name        = "Nat Gateway-2b"
-    Terraform   = "True"
-  }
-}# CREATE NAT GATEWAY in US-East-2C
-
-resource "aws_nat_gateway" "new-nat-gateway-2c" {
-  allocation_id = aws_eip.new-nat3.id
-  subnet_id     = aws_subnet.new-public-2c.id
-
-  tags = {
-    Name        = "Nat Gateway-2c"
     Terraform   = "True"
   }
 }
